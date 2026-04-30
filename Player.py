15aae6ac95f9f05d3446b13dbcde484353cd7500
Henry Slough <henry.w.slough@gmail.com>
2026-04-29 20:22:18 -0400

    Added map with custom tiles and basic player sprites

---
+++ Player.py
@@ -0,0 +1,51 @@
+from slankpy.Objects import KinematicObject
+from slankpy.Input import KeyboardInput
+
+import pygame
+
+
+class Player(KinematicObject.KinematicObject):
+    def __init__(self, width: int, height: int, *groups:pygame.sprite.Group) -> None:
+        super().__init__(width, height, *groups)
+
+        self.speed = 3
+        
+        self.sprite.add_sprites("assets/player/walk_forward.png", "walk_forward", 4, 1)
+        self.sprite.add_sprites("assets/player/walk_backward.png", "walk_backward", 4, 1)
+        self.sprite.add_sprites("assets/player/walk_left.png", "walk_left", 4, 1)
+        self.sprite.add_sprites("assets/player/walk_right.png", "walk_right", 4, 1)
+
+        self.set_sprite("walk_forward", 0)
+
+        self.direction = "forward"
+
+        self.animation_frame = 0
+        self.animation_delay = 0
+
+    def update(self) -> None:
+        
+        move_x = KeyboardInput.get_input_vector(pygame.K_a, pygame.K_d)
+        move_y = KeyboardInput.get_input_vector(pygame.K_w, pygame.K_s)
+
+        self.add_position(move_x*self.speed, move_y*self.speed)
+
+        if move_x > 0:
+            self.direction = "right" 
+        if move_x < 0:
+            self.direction = "left"
+        if move_y > 0:
+            self.direction = "forward" 
+        if move_y < 0:
+            self.direction = "backward"
+
+        if move_x != 0 or move_y != 0:
+            self.animation_delay += 1
+            if self.animation_delay >= 15:
+                self.animation_frame += 1
+                self.animation_delay = 0
+            if self.animation_frame >= 4:
+                self.animation_frame = 0
+        else:
+            self.animation_frame = 0
+
+        self.set_sprite(f"walk_{self.direction}", self.animation_frame)
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from slankpy.Screen import Screen
+from slankpy.Camera import Camera
+from slankpy.Map import MapLoader
+import Player
 
 
 screen = Screen.Screen(800, 800)
 screen.set_caption("No Country for Old Men - English Project")
 screen.set_fill_color((200, 200, 200))
+screen.add_layer("tiles")
+screen.add_layer("players")
 
 
+player = Player.Player(40, 40, screen.layers["players"])
+
+camera = Camera.Camera(player)
+camera.set_zoom(2)
+
+map_data = MapLoader.load_map("assets/untitled.tmj")
+map = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5)
+screen.layers["tiles"].add(map)
 
 running = True
 while running:
+
 
     if screen.has_quit():
         running = False
 
 
+    camera.apply_offset(*screen.layers.values())
+    screen.visible_layer = camera.cull_layers(*screen.layers.values())
     screen.update()
