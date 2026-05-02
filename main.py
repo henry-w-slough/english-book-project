4f628fcaa33b17a5d3f245e88fc2f02baf4b205d
henry-w-slough <henry.w.slough@gmail.com>
2026-05-02 17:24:26 -0400

    screw this project finally it's finished

--- main.py
+++ main.py
@@ -6,44 +6,42 @@ from slankpy.Input import MouseInput
 import Player
 import Npc
 import pygame
-import random
 
 
 screen = Screen.Screen(800, 800)
 screen.add_layer("passable")
-screen.add_layer("collides")
+screen.add_layer("collides1")
 screen.add_layer("npcs")
+screen.add_layer("collides2")
 screen.add_layer("players")
 screen.add_layer("ui")
 screen.set_fill_color((125, 104, 18))
-
-
-player = Player.Player(40, 40, screen.layers["collides"], screen.layers["players"])
-player.set_position(500, 500)
-camera = Camera.Camera(player)
-camera.set_zoom(2)
+pygame.display.set_icon(pygame.image.load("assets/no-country-for-old-men.webp"))
+screen.set_caption("No Country for Old Men - Game")
 
 
 map_data = MapLoader.load_map("assets/map.tmj")
-trim = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Floors", "Walls", "Path"])
-floor = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Trim", "Walls", "Path"])
-walls = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Floors", "Trim", "Path"])
-path = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Floors", "Trim", "Walls"])
+trim = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Floors", "Walls", "Path", "Furniture"])
+floor = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Trim", "Walls", "Path", "Furniture"])
+walls = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Floors", "Trim", "Path", "Furniture"])
+path = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Floors", "Trim", "Walls", "Furniture"])
+furniture = MapLoader.map_to_group(map_data, "assets/tileset.png", "tiles", 5, 5, ["Floors", "Trim", "Walls", "Path"])
 
 screen.layers["passable"].add(path)
-screen.layers["collides"].add(walls)
+screen.layers["collides1"].add(walls)
 screen.layers["passable"].add(floor)
-screen.layers["collides"].add(trim)
+screen.layers["collides1"].add(trim)
+screen.layers["collides2"].add(furniture)
 
 
 
-attendee = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides"])
-attendee.sprite.add_sprites("assets/npc/idle.png", "idle", 4, 1)
-attendee.sprite.add_sprites("assets/npc/hovered.png", "hovered", 4, 1)
-attendee.set_position(350, 250)
+attendee = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides1"])
+attendee.sprite.add_sprites("assets/npc/attendee/idle.png", "idle", 4, 1)
+attendee.sprite.add_sprites("assets/npc/attendee/hovered.png", "hovered", 4, 1)
+attendee.set_position(350, 220)
 attendee.dialogue = {
     0: "Well hello stranger, you come far?",
-    1: "Fuel pumps are self-serve after six, my policy.",
+    1: "I'm the attendee at this here gas station.",
     2: "I've been working this station long before your time.",
     3: "It was my father in-law's, before he passed some time ago.",
     4: "A man just came in here, he had a mighty strange haircut.",
@@ -51,9 +49,9 @@ attendee.dialogue = {
 }
 
 
-anton = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides"])
-anton.sprite.add_sprites("assets/npc/idle.png", "idle", 4, 1)
-anton.sprite.add_sprites("assets/npc/hovered.png", "hovered", 4, 1)
+anton = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides1"])
+anton.sprite.add_sprites("assets/npc/anton/idle.png", "idle", 4, 1)
+anton.sprite.add_sprites("assets/npc/anton/hovered.png", "hovered", 4, 1)
 anton.set_position(850, 220)
 anton.dialogue = {
     0: "Hello friendo... I'm Anton...",
@@ -66,47 +64,47 @@ anton.dialogue = {
 
 
 
-tom_bell = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides"])
-tom_bell.sprite.add_sprites("assets/npc/idle.png", "idle", 4, 1)
-tom_bell.sprite.add_sprites("assets/npc/hovered.png", "hovered", 4, 1)
+tom_bell = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides1"])
+tom_bell.sprite.add_sprites("assets/npc/tom_bell/idle.png", "idle", 4, 1)
+tom_bell.sprite.add_sprites("assets/npc/tom_bell/hovered.png", "hovered", 4, 1)
 tom_bell.set_position(220, 320)
 tom_bell.dialogue = {
-    0: "Hello friendo...",
-    1: "Seems we've been destined to meet here...",
-    2: "See this coin? It's here in my hand...",
-    3: "It's travelled 30 long years to get here... just for you...",
-    4: "Well... I have some business to attend too...",
-    5: "Godspeed... friendo...",
+    0: "Hey there partner, I'm Tom Bell, the sheriff.",
+    1: "Been tracking down a criminal said to be around here.",
+    2: "It's said he kills for pleasure, like sport.",
+    3: "I found a car ablaze some days back, said to be his doing.",
+    4: "Sometimes I feel the modern day is aging too fast for me.",
+    5: "But anyway, be safe out there."
 }
 
 
 
-carla_jean = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides"])
-carla_jean.sprite.add_sprites("assets/npc/idle.png", "idle", 4, 1)
-carla_jean.sprite.add_sprites("assets/npc/hovered.png", "hovered", 4, 1)
-carla_jean.set_position(760, 350)
+carla_jean = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides1"])
+carla_jean.sprite.add_sprites("assets/npc/carla_jean/idle.png", "idle", 4, 1)
+carla_jean.sprite.add_sprites("assets/npc/carla_jean/hovered.png", "hovered", 4, 1)
+carla_jean.set_position(950, 350)
 carla_jean.dialogue = {
-    0: "Hello friendo...",
-    1: "Seems we've been destined to meet here...",
-    2: "See this coin? It's here in my hand...",
-    3: "It's travelled 30 long years to get here... just for you...",
-    4: "Well... I have some business to attend too...",
-    5: "Godspeed... friendo...",
+    0: "Hello there, I'm Carla Jean.",
+    1: "My husband Llewelyn is somewhere `round here.",
+    2: "He's been gone for some days, won't even tell me what he's up to.",
+    3: "He came home yesterday with a briefcase and a gun, no explanation.",
+    4: "I sure hope my husband is safe...",
+    5: "Anyway, see you around.",
 }
 
 
 
-llewelyn = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides"])
-llewelyn.sprite.add_sprites("assets/npc/idle.png", "idle", 4, 1)
-llewelyn.sprite.add_sprites("assets/npc/hovered.png", "hovered", 4, 1)
+llewelyn = Npc.Npc(40, 40, screen.layers["npcs"], screen.layers["collides1"])
+llewelyn.sprite.add_sprites("assets/npc/llewelyn/idle.png", "idle", 4, 1)
+llewelyn.sprite.add_sprites("assets/npc/llewelyn/hovered.png", "hovered", 4, 1)
 llewelyn.set_position(300, 440)
 llewelyn.dialogue = {
-    0: "Hello friendo...",
-    1: "Seems we've been destined to meet here...",
-    2: "See this coin? It's here in my hand...",
-    3: "It's travelled 30 long years to get here... just for you...",
-    4: "Well... I have some business to attend too...",
-    5: "Godspeed... friendo...",
+    0: "Hello. Name is Llewelyn.",
+    1: "My business? Surviving best describes it.",
+    2: "I found a briefcase full of money.",
+    3: "Been on the run ever since, keeping my family safe.",
+    4: "Killer been hired to come after me, not sure of his name.",
+    5: "Keep yourself safe, friend.",
 }
 
 
@@ -118,21 +116,37 @@ dialogue_event = 0
 
 previous_input = pygame.mouse.get_pressed()
 
+
+player = Player.Player(40, 40, pygame.sprite.Group(screen.layers["collides1"], screen.layers["collides2"]), screen.layers["players"])
+player.set_position(500, 500)
+camera = Camera.Camera(player)
+camera.set_zoom(2.5)
+
+
 running = True
 while running:
-
-    screen.set_caption(f"{screen.clock.get_fps()}")
 
 
     mouse_input = pygame.mouse.get_pressed()
     
     if mouse_input[0]:
 
+        print(dialogue_event)
+
         if MouseInput.is_mouse_over_object(attendee) and dialogue_event == 0:
             attendee.active = True
             dialogue_event = 1 
         if MouseInput.is_mouse_over_object(anton) and dialogue_event == 0:
             anton.active = True
+            dialogue_event = 1 
+        if MouseInput.is_mouse_over_object(carla_jean) and dialogue_event == 0:
+            carla_jean.active = True
+            dialogue_event = 1 
+        if MouseInput.is_mouse_over_object(tom_bell) and dialogue_event == 0:
+            tom_bell.active = True
+            dialogue_event = 1 
+        if MouseInput.is_mouse_over_object(llewelyn) and dialogue_event == 0:
+            llewelyn.active = True
             dialogue_event = 1 
 
         if dialogue_event > 0 and not previous_input[0]:
@@ -143,6 +157,9 @@ while running:
                 text_box.set_text("")
                 attendee.active = False
                 anton.active = False
+                tom_bell.active = False
+                carla_jean.active = False
+                llewelyn.active = False
             else:
                 text_box.set_background_color((0, 0, 0, 200))
                 player.speed = 0
@@ -150,6 +167,12 @@ while running:
                     text_box.set_text(attendee.dialogue[dialogue_event - 1])
                 if anton.active:
                     text_box.set_text(anton.dialogue[dialogue_event - 1])
+                if tom_bell.active:
+                    text_box.set_text(tom_bell.dialogue[dialogue_event - 1])
+                if carla_jean.active:
+                    text_box.set_text(carla_jean.dialogue[dialogue_event - 1])
+                if llewelyn.active:
+                    text_box.set_text(llewelyn.dialogue[dialogue_event - 1])
                 dialogue_event += 1
 
     previous_input = mouse_input
@@ -159,6 +182,6 @@ while running:
         running = False
 
 
-    camera.apply_offset(screen.layers["players"], screen.layers["collides"], screen.layers["passable"], screen.layers["npcs"])
+    camera.apply_offset(screen.layers["players"], screen.layers["collides1"], screen.layers["collides2"], screen.layers["passable"], screen.layers["npcs"])
     screen.visible_layer = camera.cull_layers(*screen.layers.values())
     screen.update()
